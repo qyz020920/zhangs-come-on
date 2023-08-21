15a9261cbd5816e9b69b2990b33b38e6f8b348ae
qzqz <2490968788@qq.com>
2023-08-21 17:05:23 +0800

    。。。

---
+++ deploy-prd.sh
@@ -0,0 +1,36 @@
+#! /bin/bash
+
+app=bulade-admin-ui
+ds=$(date +"%Y%m%d%H%M%S")
+root_dir=/home/lizhe/workspace/$app
+deploy_dir=$root_dir/releases/$ds
+app_dir=$root_dir/application
+server=bulade-prd
+
+ssh $server << SCRIPT
+echo deploy to $deploy_dir
+mkdir -p $deploy_dir
+mkdir -p $app_dir
+ls -lha $root_dir/releases
+SCRIPT
+
+tar -czvf $app.tar.gz ./dist/*
+scp $app.tar.gz $server:$deploy_dir
+rm $app.tar.gz
+
+ssh $server << SCRIPT
+cd $deploy_dir
+tar -xvf $app.tar.gz
+rm -rf $app.tar.gz
+mv dist/* ./
+rm -rf dist $app_dir/*
+cp -r $deploy_dir/* $app_dir/
+
+chmod 755 $app_dir
+chmod 644 $app_dir/*
+chmod 755 $app_dir/assets
+chmod 644 $app_dir/assets/*
+
+echo list application dir
+ls -l $app_dir
+SCRIPT
---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<script setup>
+import HeaderMenu from "@/components/HeaderMenu.vue";
+import SideBar from "@/components/SideBar.vue";
+import { useRoute } from "vue-router";
+
+const route = useRoute();
+</script>
+
+<template>
+  <el-container>
+    <el-header class="header">
+      <el-affix position="top" :offset="0">
+        <header-menu />
+      </el-affix>
+    </el-header>
+    <el-container>
+      <el-affix position="top" :offset="60" v-if="!route.meta.loginLayout">
+        <side-bar />
+      </el-affix>
+      <el-main>
+        <router-view :key="route.fullPath" />
+      </el-main>
+    </el-container>
+  </el-container>
+</template>
+
+<style>
+.header {
+  padding: 0;
+}
+</style>
